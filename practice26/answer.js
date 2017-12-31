9c1e668012dcac9e62c7cabad254b71418838e69
m13kelore <kelceylorenzo@gmail.com>
2017-12-30 21:03:23 -0800

    completed

--- practice26/answer.js
+++ practice26/answer.js
@@ -1,1 +1,13 @@
-answer.js
+function search_array(haystack, needle){
+    var output = [];
+    for(var index = 0; index < needle.length; index++){
+        if(haystack.indexOf(needle[index]) !== -1){
+            output.push(needle[index]);
+        }
+    }
+    return output;
+}
+
+var haystack = ['cat','dog','bird','turtle','lizard'];
+var needle = ['dog','lizard','flower','monkey','unicorn'];
+console.log(search_array(haystack,needle));
