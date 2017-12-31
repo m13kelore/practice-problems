306da92003178b1853518456d84d6e5b76695660
m13kelore <kelceylorenzo@gmail.com>
2017-12-30 20:56:26 -0800

    completed

--- practice20/answer.js
+++ practice20/answer.js
@@ -1,1 +1,14 @@
-answer.js
+function randomize_list(array){
+    var output = [];
+    var z = array.length;
+    while(z > 0){
+        var index = Math.floor(Math.random() * array.length);
+        output.push(array[index]);
+        array.splice(index,1);
+        z--;
+    }
+    return output;
+}
+
+var input_array = [5,3,2,5,1];
+console.log(randomize_list(input_array));
